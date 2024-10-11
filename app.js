c5959f9f0803adfccde243525bfa47fc3eacd697
Sumaira-Abiden <sumairaabiden@gmail.com>
2024-10-11 23:31:08 +0500

    chap 1 to 3

---
+++ app.js
@@ -0,0 +1,91 @@
+// Chapter#1
+
+
+// // Question 1
+// alert("Error! please enter a valid password")
+
+// // Question 2
+// alert("Welcome to JS land...\n Happy Coding!" )
+
+// // Question 3
+// alert("Welcome to JS land...")
+
+// // Question 4
+// alert("happy Coding! \n prevent this page from creating additional dialogs.")
+
+// // Question 5 
+// alert()
+// console.log("Hello... I can run JS through my web browser's console")
+
+// Chapter#2 
+
+// Question 1
+// let username
+
+// // Question 2
+// let myName
+// myName = "Sumaira Abiden"
+// console.log(myName)
+
+// // Question 3
+// let message
+// message ="Hello World!"
+// alert(message)
+
+// // Question 4
+// let student="John Doe"
+// let age= 15
+// let course="Certified Mobile Application developer "
+
+// alert(student)
+// alert(age)
+// alert(course)
+
+// Question 5
+// let food = "PIZZA \nPIZZ \nPIZ \nPI \nP  "
+// alert(food)
+
+// Question 6
+
+// let email
+// email = "example@gmail.com"
+// alert(email)
+
+// Question 7
+
+// let book
+// book ="A smarter way to learn javascript"
+// alert(book)
+
+
+// Question 8 & 9
+// let display= "Yah! I can write HTML content through JavaScript"
+// document.write(display)
+
+// chapter 3
+// Question 1
+
+// let age
+// age = "I am 15 years old"
+// alert(age);
+
+
+// // Question 2
+
+// let visitor
+// visitor = "You have visited this site 14 times"
+// alert(visitor);
+
+// // Question 3
+
+// let birthyear
+// birthyear = " My birth year is 2004"
+// document.write(birthyear); 
+ 
+// // Question 4
+
+// let visitorName ="John Doe"
+// let productTitle="T-shirts"
+// let Quantity="5"
+// document.write(visitorName + " ordered " + Quantity + " " + productTitle + " on xyz.clothing store")
+
